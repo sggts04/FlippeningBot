8ff96eebaceb935c75b31fcece6c22ac75ebc81b
Shreyas Gupta <technology.shreyas@gmail.com>
2021-09-03 20:57:43 +0530

    remove comment

--- index.js
+++ index.js
@@ -2,11 +2,5 @@ require('dotenv').config();
 const tweetJob = require('./tweetJob');
 const mentionJob = require('./mentionJob');
 
-/*
-T.post('statuses/update', { status: 'Hello Twitter!' }, function(err, data, response) {
-    console.log(data)
-})
-*/
-
 tweetJob.start();
 mentionJob.start();
