6b4eccfab7e349968a6ea962602ae1279d4d7c9c
Shreyas Gupta <technology.shreyas@gmail.com>
2021-09-04 00:09:39 +0530

    fix mention mechanism

--- src/mentionJob.js
+++ src/mentionJob.js
@@ -2,10 +2,11 @@ const functions = require('./functions');
 
 class mentionJob {
     static async start(T) {
-        var stream = T.stream('statuses/filter', { track: ['flippeningbot'] });
-
+        var stream = T.stream('statuses/filter', { track: ['@FlippeningBot', '@Flippeningbot', '@flippeningbot'] });
+        
         stream.on('tweet', (mention) => {
-            functions.tweet(T, mention.id_str, mention.user.screen_name);
+            if(mention.text.toLowerCase().includes('update') || mention.text.toLowerCase().includes('status'))
+                functions.tweet(T, mention.id_str, mention.user.screen_name);
         });
     }
 }
